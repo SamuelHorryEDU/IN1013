8bf2419eaa4476ffc79e6618c2887187cab81f56
SamuelHorryEDU <samuel.horry@city.ac.uk>
2024-11-10 12:57:55 +0000

    homework

---
+++ HW 5/task1.sql
@@ -0,0 +1,31 @@
+-- 1
+SELECT bill_date, bill_total 
+FROM restbill 
+WHERE cust_name = 'Bob Crow';
+-- 2
+SELECT DISTINCT cust_name 
+FROM restbill 
+WHERE cust_name LIKE '%Smith' 
+ORDER BY cust_name DESC;
+-- 3
+SELECT DISTINCT cust_name 
+FROM restbill 
+WHERE cust_name LIKE '% C%' 
+ORDER BY cust_name DESC;
+-- 4
+SELECT first_name, surname
+FROM reststaff
+WHERE headwaiter IS NULL;
+-- 5
+SELECT *
+FROM restbill
+WHERE bill_date LIKE '1602%';
+-- 6
+SELECT DISTINCT bill_date
+FROM restbill
+WHERE bill_date LIKE '15%'
+ORDER BY bill_date ASC;
+
+
+
+
